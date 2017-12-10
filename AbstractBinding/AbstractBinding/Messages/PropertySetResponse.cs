02a969a2cf9a7fcd796a76b159c40e62d8a0b20a
Steven Cantrell <scantrell71@gmail.com>
2017-12-10 01:13:46 -0500

    Added missing serializable attribute

--- AbstractBinding/AbstractBinding/Messages/PropertySetResponse.cs
+++ AbstractBinding/AbstractBinding/Messages/PropertySetResponse.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace AbstractBinding.Messages
 {
+    [Serializable]
     internal class PropertySetResponse : Response
     {
 #pragma warning disable IDE1006 // Naming Styles
