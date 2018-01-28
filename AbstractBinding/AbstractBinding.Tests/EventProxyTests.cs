43668446a8df705164715857d6b1ac05b48d18ec
Steven Cantrell <astevencantrell@gmail.com>
2018-01-27 20:58:30 -0500

    Moved some initialization to constructor. Moved testing scope back in SubscribeNoArgsTest

--- AbstractBinding/AbstractBinding.Tests/EventProxyTests.cs
+++ AbstractBinding/AbstractBinding.Tests/EventProxyTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using AbstractBinding.Messages;
@@ -9,13 +11,33 @@ namespace AbstractBinding.Tests
     public class EventProxyTests
     {
         private const string _testCategory = "Event Proxy";
+        private const string _objId1 = "objId1";
+        private const string _objId2 = "objId2";
 
         private readonly Mock<IProxyClient> _clientMock;
+        private readonly Sender _sender;
 
         public EventProxyTests()
         {
             // Initialize client mock
             _clientMock = new Mock<IProxyClient>();
+            _clientMock.Setup(o => o.Request(It.IsAny<GetBindingDescriptionsRequest>())).Returns<GetBindingDescriptionsRequest>(req =>
+            {
+                return new GetBindingDescriptionsResponse()
+                {
+                    bindings = new Dictionary<string, ObjectDescription>()
+                    {
+                        { _objId1, ObjectDescriptor.GetObjectDescription<IRegisteredObject>() },
+                        { _objId2, ObjectDescriptor.GetObjectDescription<IRegisteredObject2>() }
+                    }
+                };
+            });
+
+            // Initialize sender
+            _sender = new Sender(_clientMock.Object);
+            _sender.Register<IRegisteredObject>();
+            _sender.Register<IRegisteredObject2>();
+            _sender.SynchronizeBindings();
         }
 
         [TestMethod]
@@ -23,10 +45,9 @@ namespace AbstractBinding.Tests
         public void SubscribeNoArgsTest()
         {
             // Arrange
-            string objectId = "objId1";
             var expectedRequest = new SubscribeRequest()
             {
-                objectId = objectId,
+                objectId = _objId1,
                 eventId = ObjectDescriptor.GetEventId<IRegisteredObject>(nameof(IRegisteredObject.NotifyOnNonDataChanged))
             };
             SubscribeRequest actualRequest = null;
@@ -36,11 +57,12 @@ namespace AbstractBinding.Tests
 
                 return new SubscribeResponse()
                 {
-                    objectId = objectId,
+                    objectId = _objId1,
                     eventId = ObjectDescriptor.GetEventId<IRegisteredObject>(nameof(IRegisteredObject.NotifyOnNonDataChanged))
                 };
             });
-            var objProxy = RuntimeProxy.Create<IRegisteredObject>(objectId, _clientMock.Object);
+
+            var objProxy = _sender.GetBindingsByType<IRegisteredObject>()[_objId1];
 
             // Act
             objProxy.NotifyOnNonDataChanged += (s, e) => { };
